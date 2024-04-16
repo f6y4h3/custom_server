f51f396301994d867729ff1d1c5665cbfb98f162
fyh <3285206802@qq.com>
2024-04-16 11:19:42 +0800

    'fix

--- src/public/public.controller.ts
+++ src/public/public.controller.ts
@@ -1,48 +1,3 @@
-// import {
-//   Controller,
-//   Post,
-//   UseInterceptors,
-//   UploadedFile,
-//   BadRequestException
-// } from '@nestjs/common';
-
-// import { FileInterceptor } from '@nestjs/platform-express';
-// import { Express } from 'express';
-// import * as path from 'path';
-// import { fileStorage } from '../utils/file-storage';
-
-// @Controller('public')
-// export class PublicController {
-//   @Post('upload')
-//   @UseInterceptors(
-//     FileInterceptor('picture', {
-//       dest: 'uploads',
-//       storage: fileStorage,
-//       // 限制图片大小
-//       limits: {
-//         fileSize: 1024 * 1024 * 10 // 10M
-//       },
-//       // 限制图片格式
-//       fileFilter(req, file, callback) {
-//         const extname = path.extname(file.originalname);
-//         console.log(extname,'extname');
-//         if (['.png', '.jpg', '.gif', '.jpeg'].includes(extname)) {
-//           console.log('000000');
-
-//           callback(null, true);
-//         } else {
-//           console.log('只能上传图片！');
-
-//           callback(new BadRequestException('只能上传图片！'), false);
-//         }
-//       }
-//     })
-//   )
-//   uploadPicture(@UploadedFile() file: Express.Multer.File) {
-//     console.log(file,'uploadPicture');
-//     return file;
-//   }
-// }
 import {
   Controller,
   Post,
@@ -51,8 +6,8 @@ import {
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { PublicService } from './public.service';
-// import { diskStorage } from 'multer';
-// import { extname } from 'path';
+import config from '../common/config';
+
 @Controller('public')
 export class PublicController {
   constructor(private readonly publicService: PublicService) {}
@@ -71,10 +26,8 @@ export class PublicController {
   // )
   @UseInterceptors(FileInterceptor('file'))
   upload(@UploadedFile() file: Express.Multer.File) {
-    // console.log('upload', file); // return this.uploadService.upload();
-    // file.path
     const fileResult = {
-      path: `http://localhost:16000/static/${file.filename}`,
+      path: `${config.path}/static/${file.filename}`,
       name: file.filename,
       size: file.size,
     };
