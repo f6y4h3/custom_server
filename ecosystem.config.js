93405df9fd23591aeab0c319f61ff6999782678e
fyh <3285206802@qq.com>
2024-04-16 10:25:53 +0800

    'fix'

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'nestjs',
-      script: './src/main.ts',
+      script: 'dist/main.js',
       instances: 1,  // 根据需求设置实例数量
       exec_mode: 'fork',
       autorestart: true,
